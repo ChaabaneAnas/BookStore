9b20118044794fa180a54d5ea044b412343f0e0d
Anas <anas.chaabane98@gmail.com>
2022-09-21 15:51:49 +0100

    add&remove-func

--- src/Components/BookList.js
+++ src/Components/BookList.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const BookList = () => {
-  const [books, setBooks] = useState([
-    { id: uuidv4(), title: 'jmall', author: 'anas' },
-    { id: uuidv4(), title: 'title', author: 'author' },
-    { id: uuidv4(), title: 'title', author: 'author' },
-  ]);
+  const books = useSelector((state) => state.bookReducer);
 
   return (
     <ul>
-      { books.map((book) => <Book key={book.id} title={book.title} author={book.author} />)}
+      { books.map((book) => (
+        <Book
+          key={book.id}
+          title={book.title}
+          author={book.author}
+          id={book.id}
+        />
+      ))}
     </ul>
   );
 };
